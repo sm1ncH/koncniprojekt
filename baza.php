4d98af3c958ee46cce5a9a5747972f7c35cad0cf
sm1ncH <78959537+sm1ncH@users.noreply.github.com>
2023-06-11 21:38:18 +0200

    poprava za bazzo

--- baza.php
+++ baza.php
@@ -1,19 +1,9 @@
 <?php
 /* povezava na bazo */
 $host='localhost';
-$user= 'root';
-$password='';
-$db='projekt';
+$user= 'jurep_db';
+$password='mawnez-cyfrYs-besxa5';
+$db='jurep_koncniprojekt';
 $link = mysqli_connect($host, $user, $password, $db)
         or die("povezava na strežnik ni uspela");
 mysqli_set_charset($link, "utf-8");
-
-// <?php
-// /* povezava na bazo */
-// $host='localhost';
-// $user= 'jurep_db';
-// $password='mawnez-cyfrYs-besxa5';
-// $db='jurep_koncniprojekt';
-// $link = mysqli_connect($host, $user, $password, $db)
-//         or die("povezava na strežnik ni uspela");
-// mysqli_set_charset($link, "utf-8");
